595ed38b8280de6fe703d0a7ca5e70d615a80194
Fahrul Yurisnan <fahrul4215@gmail.com>
2018-10-22 07:06:59 +0700

    admin logout + fix recycler view + detail user

--- app/src/main/java/com/iqbalfahrul/kelompok10/carijodoh/AdminDashboardActivity.java
+++ app/src/main/java/com/iqbalfahrul/kelompok10/carijodoh/AdminDashboardActivity.java
@@ -1,14 +1,19 @@
 package com.iqbalfahrul.kelompok10.carijodoh;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -31,6 +36,27 @@ public class AdminDashboardActivity extends AppCompatActivity {
         tampilRecyclerView();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate dari menu; disini akan menambahkan item menu pada Actionbar
+        getMenuInflater().inflate(R.menu.menu, menu);//Memanggil file bernama menu di folder menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                Toast.makeText(getApplicationContext(),"Berhasil Log Out",Toast.LENGTH_SHORT).show();
+                resetPreferences();
+                Intent intent = new Intent(this.getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void tampilRecyclerView() {
         rv = findViewById(R.id.recyclerView);
         rv.setLayoutManager(new GridLayoutManager(this,2));
@@ -41,7 +67,7 @@ public class AdminDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view, final int position) {
                 final DataUser du = dataUsers.get(position);
-                final CharSequence[] dialogitem = {"Lihat User", "Update User", "Hapus User"};
+                final CharSequence[] dialogitem = {"Lihat User", "Hapus User"};
                 AlertDialog.Builder builder = new AlertDialog.Builder(AdminDashboardActivity.this);
 
                 builder.setTitle("Pilihan");
@@ -50,24 +76,25 @@ public class AdminDashboardActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         switch (which){
                             case 0 :
-//                                Intent intent0 = new Intent(getApplicationContext(),
-//                                        ViewMovie.class);
-//                                intent0.putExtra("judul", selection);
-//                                startActivity(intent0);
+                                Intent intent0 = new Intent(getApplicationContext(),
+                                        DetailUser.class);
+                                intent0.putExtra("nama", du.getNama());
+                                intent0.putExtra("username", du.getUsername());
+                                intent0.putExtra("jkl", du.getJkl());
+                                intent0.putExtra("alamat", du.getAlamat());
+                                intent0.putExtra("foto", du.getFoto());
+                                startActivity(intent0);
                                 break;
                             case 1 :
-//                                Intent intent1 = new Intent(getApplicationContext(),
-//                                        UpdateMovie.class);
-//                                intent1.putExtra("judul", selection);
-//                                startActivity(intent1);
-                                break;
-                            case 2 :
                                 SQLiteDatabase db = dataHelper.getWritableDatabase();
                                 db.delete("user","_id = '"+ du.getId() +"'",
                                         null);
                                 Toast.makeText(getApplicationContext(),
                                         "User Berhasil Dihapus", Toast.LENGTH_LONG).show();
-                                tampilRecyclerView();
+                                finish();
+                                overridePendingTransition(0, 0);
+                                startActivity(getIntent());
+                                overridePendingTransition(0, 0);
                                 break;
                         }
                     }
@@ -83,7 +110,6 @@ public class AdminDashboardActivity extends AppCompatActivity {
     }
 
     private void isiDataUser(){
-        DataUser dataUser = new DataUser();
         dataHelper = new MyDataHelper(this);
         SQLiteDatabase db = dataHelper.getReadableDatabase();
 
@@ -92,6 +118,7 @@ public class AdminDashboardActivity extends AppCompatActivity {
 
         for (int cc=0;cc < cursor.getCount();cc++){
             cursor.moveToPosition(cc);
+            DataUser dataUser = new DataUser();
             dataUser.setId(cursor.getInt(0));
             dataUser.setNama(cursor.getString(1));
             dataUser.setUsername(cursor.getString(2));
@@ -102,5 +129,12 @@ public class AdminDashboardActivity extends AppCompatActivity {
             dataUser.setLevel(cursor.getInt(7));
             dataUsers.add(dataUser);
         }
+    }
+
+    private void resetPreferences(){
+        SharedPreferences handler = getSharedPreferences("login",MODE_PRIVATE);
+        SharedPreferences.Editor editor = handler.edit();
+        editor.clear();
+        editor.apply();
     }
 }
