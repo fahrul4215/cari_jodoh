2ff37cdf014c5287fdcb883812a1fbd156fb67ca
Fahrul Yurisnan <fahrul4215@gmail.com>
2018-10-22 10:25:03 +0700

    FIX

--- app/src/main/java/com/iqbalfahrul/kelompok10/carijodoh/RecyclerTouchListener.java
+++ app/src/main/java/com/iqbalfahrul/kelompok10/carijodoh/RecyclerTouchListener.java
@@ -1,6 +1,7 @@
 package com.iqbalfahrul.kelompok10.carijodoh;
 
 import android.content.Context;
+
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
